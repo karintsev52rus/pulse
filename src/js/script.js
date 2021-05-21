1b59a9f855dd2478a858c164050af2853a2afc74
karintsev52rus <karintsevp@gmail.com>
2021-05-21 21:32:16 +0300

    add tabs

--- src/js/script.js
+++ src/js/script.js
@@ -15,5 +15,26 @@ $(document).ready(function(){
             }
             
           }
-      ]}
-  )})
+      ]}),
+      $('ul.catalog__tabs').on('click', 'li:not(.tab_active)', function() {
+        $(this)
+          .addClass('tab_active').siblings().removeClass('tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+
+        $('.catalog-item__link').each(function(i){
+          $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+        });
+        $('.catalog-item__link_back').each(function(i){
+          $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+        })
+        
+})

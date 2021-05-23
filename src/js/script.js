4ba8418ace0f61769796b9ec65b18a5557c87fdc
karintsev52rus <karintsevp@gmail.com>
2021-05-23 22:56:51 +0300

    correct tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.slider__inner').slick({
-      autoplay: true,
+      autoplay: false,
       prevArrow: '<button type="button" class="slick-prev"><img src="../img/img-chevron-left.png"></button>',
       nextArrow:'<button type="button" class="slick-next"><img src="../img/img-chevron-right.png"></button>',
       dots: false,
